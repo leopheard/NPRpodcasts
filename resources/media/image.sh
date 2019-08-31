7c13c933d33f2286c25608b3f42557648ca05609
leopheard <leopheard.misc@gmail.com>
2019-08-31 16:07:18 -0400

    uploads

--- resources/media/image.sh
+++ resources/media/image.sh
@@ -1,5 +1,7 @@
 convert -resize 512x512 icon.jpg icon.jpg
 
+convert -resize 512x512 icon.jpeg icon.jpg
+
 convert -resize 512x512 logo.jpg icon.jpg
 
 convert -resize 512x512 logo.png icon.jpg
@@ -8,21 +10,23 @@ convert -resize 512x512 1.jpg 1.jpg
 
 convert -resize 512x512 1.png 1.png
 
-convert -resize 512x512 1.jpeg 1.jpeg
+convert -resize 512x512 1.jpeg 1.jpg
 
 convert -resize 512x512 1.jpg 1.jpg
 
 convert -resize 512x512 1.png 1.png
 
-convert -resize 512x512 1.jpeg 1.jpeg
+convert -resize 512x512 1.jpeg 1.jpg
 
 convert -resize 512x512 1.jpg 1.jpg
 
 convert -resize 512x512 1.png 1.png
 
-convert -resize 512x512 1.jpeg 1.jpeg
+convert -resize 512x512 1.jpeg 1.jpg
 
 convert -resize 1280x720 fanart.jpg fanart.jpg
+
+convert -resize 1280x720 fanart.jpeg fanart.jpg
 
 convert -resize 1280x720 fanart.png fanart.jpg
 
